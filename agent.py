edfdf31ab76bd5409970323d234a37d3a2654541
Rok <rok.ankaran@gmail.com>
2023-06-25 13:04:25 +0200

    created train function

--- agent.py
+++ agent.py
@@ -25,14 +25,52 @@ class Agent:
     def train_long_memory(self):
         pass
 
-    def train_short_memory(self):
+    def train_short_memory(self, state, action, reward, next_state, done):
         pass
 
     def get_action(self, state):
         pass
 
 def train():
-    pass
+    plot_scores = []
+    plot_mean_scores = []
+    total_score = 0
+    record = 0
+    agent = Agent()
+    game = SnakeGameAI()
+    while True:
+        #get old state
+        state_old = agent.get_state()
+
+        #get move
+        final_move = agent.get_action(state_old)
+
+        #perform move
+        reward, done, score = game.play_step(final_move)
+
+        #get new state
+        state_new = agent.get_state()
+
+        #train short memory
+        agent.train_short_memory(state_old, final_move, reward, state_new, done)
+
+        #rememger
+        agent.remember(state_old, final_move, reward, state_new, done)
+
+        if done:
+            #train long memory, plot results
+            game.reset()
+            agent.number_of_games += 1
+            agent.train_long_memory()
+
+            if score > record:
+                record = score
+                #TODO: agent.model.save()
+
+            print('Game:', agent.number_of_games, '  Score:', score, '  Record:', record)
+
+            #TODO: plot
+
 
 if __name__ == '__main__':
     train()
